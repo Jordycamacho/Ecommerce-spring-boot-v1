e6bc66caf4961b40240e8d0e56d02cca5ccf7fac
Jordy Camacho <jordycamacho225@gmail.com>
2024-06-13 23:56:25 +0200

    [FIX] arreglo de estilos en banners, inicio y responsive

--- proyectofinal.sql
+++ proyectofinal.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 12-06-2024 a las 16:24:51
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 13-06-2024 a las 21:54:55
+-- Versión del servidor: 8.0.33
+-- Versión de PHP: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,13 +27,15 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `contacto`
 --
 
-CREATE TABLE `contacto` (
-  `id` bigint(20) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
+DROP TABLE IF EXISTS `contacto`;
+CREATE TABLE IF NOT EXISTS `contacto` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
   `fecha_creada` datetime(6) DEFAULT NULL,
-  `mensaje` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `numero` varchar(255) DEFAULT NULL
+  `mensaje` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `nombre` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `numero` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 -- --------------------------------------------------------
@@ -42,15 +44,28 @@ CREATE TABLE `contacto` (
 -- Estructura de tabla para la tabla `detalles`
 --
 
-CREATE TABLE `detalles` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `detalles`;
+CREATE TABLE IF NOT EXISTS `detalles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `cantidad` double NOT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
   `precio` double NOT NULL,
   `total` double NOT NULL,
-  `orden_id` bigint(20) DEFAULT NULL,
-  `producto_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+  `orden_id` bigint DEFAULT NULL,
+  `producto_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKdurdo71oa161lmmal7oeaor74` (`orden_id`),
+  KEY `FKio4oyl8qt5jclekxp7bwws2iy` (`producto_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `detalles`
+--
+
+INSERT INTO `detalles` (`id`, `cantidad`, `nombre`, `precio`, `total`, `orden_id`, `producto_id`) VALUES
+(4, 1, 'Marvel Spider Man 2', 64.84, 64.84, 5, 20),
+(5, 1, 'Red Dead Redemption 2', 18.15, 18.15, 5, 23),
+(6, 1, 'Hellblade II', 36.99, 36.99, 5, 18);
 
 -- --------------------------------------------------------
 
@@ -58,14 +73,17 @@ CREATE TABLE `detalles` (
 -- Estructura de tabla para la tabla `ordenes`
 --
 
-CREATE TABLE `ordenes` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `ordenes`;
+CREATE TABLE IF NOT EXISTS `ordenes` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `fecha_creada` datetime(6) DEFAULT NULL,
   `fecha_recibida` datetime(6) DEFAULT NULL,
-  `numero` varchar(255) DEFAULT NULL,
+  `numero` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
   `total` double NOT NULL,
-  `usuario_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+  `usuario_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKsqu43gsd6mtx7b1siww96324` (`usuario_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `ordenes`
@@ -75,7 +93,8 @@ INSERT INTO `ordenes` (`id`, `fecha_creada`, `fecha_recibida`, `numero`, `total`
 (1, '2024-06-10 11:07:46.000000', NULL, '0000000001', 5, 1),
 (2, '2024-06-10 11:13:17.000000', NULL, '0000000002', 15, 2),
 (3, '2024-06-10 11:16:54.000000', NULL, '0000000003', 5, 2),
-(4, '2024-06-10 11:16:55.000000', NULL, '0000000004', 0, 2);
+(4, '2024-06-10 11:16:55.000000', NULL, '0000000004', 0, 2),
+(5, '2024-06-13 17:24:11.726000', NULL, '0000000005', 119.98, 1);
 
 -- --------------------------------------------------------
 
@@ -83,28 +102,34 @@ INSERT INTO `ordenes` (`id`, `fecha_creada`, `fecha_recibida`, `numero`, `total`
 -- Estructura de tabla para la tabla `productos`
 --
 
-CREATE TABLE `productos` (
-  `id` bigint(20) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `categoria` varchar(255) DEFAULT NULL,
-  `descripcion` varchar(255) DEFAULT NULL,
-  `imagen` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `precio` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `cantidad` int NOT NULL,
+  `categoria` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `descripcion` varchar(700) CHARACTER SET utf16 COLLATE utf16_unicode_ci DEFAULT NULL,
+  `imagen` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `nombre` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `precio` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
 INSERT INTO `productos` (`id`, `cantidad`, `categoria`, `descripcion`, `imagen`, `nombre`, `precio`) VALUES
-(8, 10, 'xbox', ' ultimo call of duty espero que al fin este sea bueno', 'black ops 6.jpg', 'Black ops 6', 69.99),
-(9, 10, 'xbox', ' descripcion de prueba videojuegos de x-box', 'Colorful Boi.png', 'Colorful Boi', 39.99),
-(10, 10, 'xbox', 'Descripción de prueba para video juegos de x-box', 'Story time bundle.png', 'Story time bundle', 49.99),
-(11, 10, 'xbox', 'Descripción de prueba para video juegos de x-box', 'the hong kong massacre.jpg', 'The Hong Kong Massacre', 49.99),
-(12, 10, 'inicio', 'Descripción de prueba para video juegos de x-box', 'grapping.jpg', 'Grapping', 25.99),
-(13, 10, 'inicio', 'Ya dejen de sacar putos Assassing`s creed dejenlo moriri', 'Assassins creed Shadows.jpg', 'Assassins creed Shadows', 65.99),
-(14, 10, 'inicio', ' descripcion de prueba para productos de ps ', 'Rise of the Ronin.jpg', 'Rise of the Ronin', 95.99);
+(15, 10, 'xbox', ' Adéntrate en el peligroso mundo abierto de Skull and Bones, una experiencia cooperativa de rol y acción, para convertirte en un temible cabecilla pirata. Participa en emocionantes combates navales, fabrica barcos únicos y forja alianzas inverosímiles mientras superas retos y desatas el caos en los mares.', 'xbox-skull-and-bones.jpg', 'Skull and Bones', 39.99),
+(16, 10, 'xbox', 'Forza Motorsport para PC es el equivalente a Forza Motorsport 8, pero es más un reinicio del juego original que una continuación: de ahí que el nombre no lleve número. Es un videojuego de simulación de carreras de coches.', 'xbox-forza-motorsport.jpg', 'Forza Motor', 34.29),
+(17, 10, 'xbox', 'Starfield es el primer universo nuevo en más de 25 años de Bethesda Game Studios, los galardonados creadores de The Elder Scrolls V: Skyrim y Fallout 4.', 'xbox-starfield.jpg', 'Starfield', 58.99),
+(18, 9, 'xbox', 'En esta secuela del galardonado Hellblade: Senua\'s Sacrifice, Senua regresa en un brutal viaje de supervivencia a través de los mitos y tormentos de la Islandia vikinga.', 'xbox-hellblade-II.jpg', 'Hellblade II', 36.99),
+(19, 10, 'playstation', 'ADAPTARSE O MORIR EN LA LUCHA CONTRA LA AMENAZA DEFINITIVA En la secuela directa del exitoso juego Call of Duty®: Modern Warfare® II, el capitán Price y la Fuerza operativa 141 se enfrentan a la amenaza definitiva.', 'playstation-modern-warfare-iii.jpg', 'Modern Warfare III', 69.09),
+(20, 9, 'playstation', 'Marvel Spider-Man 2 para PlayStation es un juego de acción y aventuras para un jugador que presenta a Miles Morales y Peter Parker como el superhéroe del mismo nombre. ', 'playstation-spider-man-2.jpg', 'Marvel Spider Man 2', 64.84),
+(21, 10, 'playstation', 'THE NORSE SAGA CONTINUES From Santa Monica Studio and brought to PC in partnership with Jetpack Interactive comes God of War Ragnarök, an epic and heartfelt journey that follows Kratos and Atreus as they struggle with holding on and letting go.', 'playstation-god-of-war-ragnarok.jpg', 'God Of War Ragnarok', 48.41),
+(22, 10, 'playstation', 'Llega a nuevas cotas Disfruta del majestuoso mundo de Horizon como nunca antes en esta nueva aventura para PlayStation®VR2.', 'playstation-horizon-call-of-the-mountain.jpg', 'Horizon Call Of The Mountain', 43.99),
+(23, 29, 'inicio', 'Red Dead Redemption 2 para PC es un juego de acción y aventuras de mundo abierto en el que el jugador puede vagar libremente. Cuenta algunos elementos en tercera persona, además de otras modalidades de juego en primera persona.', 'red-dead-redemption-2.jpg', 'Red Dead Redemption 2', 18.15),
+(24, 10, 'inicio', 'El paquete prémium incluye el siguiente contenido: Expansión ELDEN RING - Shadow of the Erdtree Libro de arte y banda sonora de ELDEN RING - Shadow of the Erdtree ELDEN RING', 'inicio-elden-ring.jpg', 'Elden Ring', 38.69),
+(25, 10, 'inicio', 'La edición incluye: Conjunto de armadura \"Salvador de los libres DP-53\". Capa \"La voluntad del Pueblo\". Arma \"Caballero MP-98\". Condición de Superciudadano. Juego de nave de héroe de las estratagemas. Bono de guerra prémium \"Veteranos avezados\".', 'inicio-helldivers-2.jpg', 'Helldivers II', 42.89);
 
 -- --------------------------------------------------------
 
@@ -112,14 +137,16 @@ INSERT INTO `productos` (`id`, `cantidad`, `categoria`, `descripcion`, `imagen`,
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` bigint(20) NOT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `tipo` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `nombre` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  `tipo` varchar(255) COLLATE utf16_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf16 COLLATE=utf16_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -128,77 +155,6 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `direccion`, `email`, `nombre`, `password`, `tipo`) VALUES
 (1, 'narnia', 'jordycamacho225@gmail.com', 'Jordy', '1234', 'ADMIN'),
 (2, 'narnia', 'prueba@gmail.com', 'Prueba', '$2a$10$nEyLxO.WtvCpM8.F0NxRLOV4xll83ZQs8M9m1Ozlmdzn35nreWtKG', 'USER');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `contacto`
---
-ALTER TABLE `contacto`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `detalles`
---
-ALTER TABLE `detalles`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKdurdo71oa161lmmal7oeaor74` (`orden_id`),
-  ADD KEY `FKio4oyl8qt5jclekxp7bwws2iy` (`producto_id`);
-
---
--- Indices de la tabla `ordenes`
---
-ALTER TABLE `ordenes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `FKsqu43gsd6mtx7b1siww96324` (`usuario_id`);
-
---
--- Indices de la tabla `productos`
---
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `contacto`
---
-ALTER TABLE `contacto`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `detalles`
---
-ALTER TABLE `detalles`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `ordenes`
---
-ALTER TABLE `ordenes`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `productos`
---
-ALTER TABLE `productos`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Restricciones para tablas volcadas
