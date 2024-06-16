af7f419586250438f59e1bff24105f31c8576304
Jordy Camacho <jordycamacho225@gmail.com>
2024-06-16 11:29:53 +0200

    eliminacion de todo

--- src/main/java/com/example/tienda/service/IDetalleOrdenService.java
+++
@@ -1,9 +0,0 @@
-package com.example.tienda.service;
-
-import com.example.tienda.model.DetalleOrden;
-
-public interface IDetalleOrdenService {
-	
-	DetalleOrden save (DetalleOrden detalleOrden);
-
-}
